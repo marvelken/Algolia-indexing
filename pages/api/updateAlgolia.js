6373e78e9847e7b6a6d3dab0cbeeab64eca655d3
marvelken <marvelken2000@gmail.com>
2024-06-15 09:45:02 +0100

    added changes

--- pages/api/updateAlgolia.js
+++ pages/api/updateAlgolia.js
@@ -1,40 +1,39 @@
 // pages/api/updateAlgolia.js
-import algoliasearch from 'algoliasearch';
+import algoliasearch from "algoliasearch";
 
 // Initialize the Algolia client
-const client = algoliasearch('T19D32MYUL', '5f5ca9e0f562329fb2cc6a00c3712141');
-const index = client.initIndex('Directus');
+const client = algoliasearch("T19D32MYUL", "5f5ca9e0f562329fb2cc6a00c3712141");
+const index = client.initIndex("Directus_Events");
 
-
+// API handler to update the Algolia client
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ message: 'Method not allowed' });
-    }
-  
-    const { trigger, records } = req.body;
-  
+  if (req.method === "POST") {
     try {
-      if (!Array.isArray(records)) {
-        throw new Error('Records should be an array');
+      console.log(req.body, "show the body");
+      if (req.body.event === "Blog.items.create") {
+        await index.saveObjects([
+          { objectID: `${req.body.key}`, ...req.body.payload },
+        ]);
+      } else if (req.body.event === "Blog.items.update") {
+        console.log("show the updated data", req.body);
+        await Promise.all(
+          req.body.keys.map(
+            async (key) =>
+              await index.partialUpdateObjects([
+                { objectID: `${key}`, ...req.body.payload },
+              ])
+          )
+        );
+      } else if (req.body.event === "Blog.items.delete") {
+        console.log("show the deleted data", req.body);
+        await index.deleteObjects(req.body.keys);
       }
-  
-      switch (trigger) {
-        case 'create':
-          await index.saveObjects(records);
-          break;
-        case 'update':
-          await index.partialUpdateObjects(records);
-          break;
-        case 'delete':
-          const objectIDs = records.map(record => record.id);
-          await index.deleteObjects(objectIDs);
-          break;
-        default:
-          throw new Error('Unsupported trigger');
-      }
-  
-      res.status(200).json({ message: 'Success' });
+
+      res.status(200).json({ message: "Success" });
     } catch (error) {
-      res.status(500).json({ message: 'Error updating Algolia', error: error.message });
+      res
+        .status(500)
+        .json({ message: "Error updating Algolia", error: error.message });
     }
-  }
+  } else return res.status(400).json({ message: "Method not allowed" });
+}
